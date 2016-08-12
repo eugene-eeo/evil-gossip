77f46a2aefa33a066f8bbf572614c66961c002d5
Eeo Jun <141bytes@gmail.com>
2016-08-12 23:43:52 +0800

    test using hypothesis

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,6 @@
 from collections import Counter
+from hypothesis import given
+import hypothesis.strategies as st
 from evil_gossip.utils import edges, full_dist, argmax, sparse_dist
 
 
@@ -7,22 +9,28 @@ def test_argmax():
     assert argmax(Counter(a=1, b=1, c=0)) in ['a', 'b']
 
 
-def test_full_dist():
-    assert dict(full_dist([1,2,3])) == {
-        1: [2, 3],
-        2: [1, 3],
-        3: [1, 2],
-    }
+@given(st.lists(st.integers(), unique=True))
+def test_full_dist(xs):
+    x = set(xs)
+    for u, v in full_dist(xs):
+        assert u not in v
+        assert {u} | set(v) == x
 
 
-def test_edges():
-    assert list(edges([1, 2, 3])) == [(1, 2), (1, 3), (2, 3)]
-
-
-def test_edges_property():
-    v = list(edges([1, 2, 3, 4, 5]))
+@given(st.lists(st.integers(), unique=True))
+def test_edges_property(xs):
+    v = list(edges(xs))
     s = set(frozenset([a, b]) for a, b in v)
     assert len(v) == len(s)
+
+
+
+@given(st.lists(st.integers(), unique=True))
+def test_sparse_dist(xs):
+    d = dict(sparse_dist(xs))
+    for a in d:
+        for b in d[a]:
+            assert a in d[b]
 
 
 def test_sparse_dist():
