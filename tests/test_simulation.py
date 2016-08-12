c27402b53988b32391df17249db2904d4990029e
Eeo Jun <141bytes@gmail.com>
2016-08-12 23:59:32 +0800

    test return value

--- tests/test_simulation.py
+++ tests/test_simulation.py
@@ -8,10 +8,17 @@ def test_broadcast():
     mailbox = defaultdict(Counter)
     node = EvilNode(message)
     node.links = [1, 2, 3]
-    broadcast(node, mailbox)
 
+    assert broadcast(node, mailbox) == message
     assert mailbox == {
         1: {message: 1},
         2: {message: 1},
         3: {message: 1},
     }
+
+
+def test_broadcast_empty():
+    message = 0
+    mailbox = defaultdict(Counter)
+    node = EvilNode(message)
+    assert broadcast(node, mailbox) is None
