fe5ac029dbc538a26a277e375db48ea25d0b39b2
Eeo Jun <141bytes@gmail.com>
2016-08-12 23:58:11 +0800

    check that broadcast helper works

---
+++ tests/test_simulation.py
@@ -0,0 +1,17 @@
+from collections import defaultdict, Counter
+from evil_gossip.simulation import broadcast
+from evil_gossip.node import EvilNode
+
+
+def test_broadcast():
+    message = 0
+    mailbox = defaultdict(Counter)
+    node = EvilNode(message)
+    node.links = [1, 2, 3]
+    broadcast(node, mailbox)
+
+    assert mailbox == {
+        1: {message: 1},
+        2: {message: 1},
+        3: {message: 1},
+    }
