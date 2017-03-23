709f244daa82b86d4e3986237cd6b77df8375fe8
Eeo Jun <141bytes@gmail.com>
2017-03-23 20:57:38 +0800

    refresh project and update

--- README.rst
+++ README.rst
@@ -1,53 +1,34 @@
-evil-gossip
-===========
+evil-gossip (WIP)
+=================
 
 a simulation to find out which model - fully or sparsely connected provides
 better tolerance against multiple adversaries who can actively publish messages
 using a gossip protocol.
 
-* target: get all good nodes to agree that some value ``v`` is the correct
-  message, within a number of ticks ``t``.
-* all nodes (good and evil) broadcast at the same time each tick.
-* good nodes:
+* target: get all neutral nodes to agree that ``v`` is the correct message,
+  within a number of ticks ``t``.
+* all nodes broadcast at the same time each tick.
+* neutral nodes:
 
-  * keeps an internal counter of messages.
+  * keep an internal counter of messages.
   * will bump the corresponding entry in the counter upon receiving.
   * always sends the message with the highest count.
 
-* evil nodes:
+* good/evil nodes:
 
-  * always sends a different message ``!= v``.
+  * good always sends ``v``.
+  * evil always sends a different message ``!= v``.
   * in this implementation the potency of the evil nodes is necessarily
     maximised as they all send the identical evil message.
 
-* once only ``v`` is in circulation (by the good nodes) then the good
+* once only ``v`` is in circulation (by the neutral nodes) then the good
   nodes have won.
-* else once ``t`` is used up they have lost.
+* once ``t`` is used up they have lost.
 
+default parameters:
 
-usage
-~~~~~
-
-.. code-block:: shell
-
-    # install dependencies
-    $ make cpython   # or if you're feeling brave: make pypy
-    $ ./evil-gossip
-
-
-dev
-~~~
-
-.. code-block:: shell
-
-    $ git clone ssh://git@github.com/eugene-eeo/evil-gossip.git
-    $ cd evil-gossip
-    $ vim
-    $ make test
-
-
-todo
-~~~~
-
-* generate nice graphs and PDFs
-* rewrite in Go just for fun
+* **0 < p ≤ 1.0** (p = prob. of a link between two nodes)
+* **K = 10** (K = no. of good nodes)
+* **N = 100** (N = no. of neutral nodes)
+* **10 ≤ B ≤ 50** (B = no. of evil nodes)
+* **t = 500**
---
+++ evil_gossip.py
@@ -0,0 +1,98 @@
+import random
+from collections import Counter, defaultdict
+
+
+GOOD_MSG = True
+EVIL_MSG = False
+
+
+def argmax(c):
+    key, _ = c.most_common(1)[0]
+    return key
+
+
+class Acceptor:
+    def __init__(self):
+        self.peers = []
+        self.counter = Counter()
+
+    def broadcast(self):
+        if self.counter:
+            return argmax(self.counter), self.peers
+        return None, []
+
+    def update(self, messages):
+        self.counter.update(messages)
+
+
+class Proposer:
+    def __init__(self, value):
+        self.peers = []
+        self.value = value
+
+    def broadcast(self):
+        return self.value, self.peers
+
+    def update(self, _):
+        pass
+
+
+def full_dist(xs):
+    for i, v in enumerate(xs):
+        yield v, xs[:i] + xs[i+1:]
+
+
+def sparse_dist(xs, p, entropy=random.random):
+    for node, peers in full_dist(xs):
+        yield node, [p for p in peers if entropy() <= p]
+
+
+def allocate(N, K, B):
+    B = [Proposer(EVIL_MSG) for _ in range(B)]
+    B.extend(Proposer(GOOD_MSG) for _ in range(K))
+    N = [Acceptor() for _ in range(N)]
+    return B, N
+
+
+def add_links(nodes):
+    for node, peers in full_dist(nodes):
+        node.peers = peers
+
+
+def broadcast(node, mailbox):
+    message, peers = node.broadcast()
+    if message is not None:
+        for p in peers:
+            mailbox[p][message] += 1
+        return message
+    return None
+
+
+def update_all(mailbox):
+    for receiver in mailbox:
+        receiver.update(mailbox[receiver])
+
+
+def simulate(proposers, acceptors, t=500):
+    mailbox = defaultdict(Counter)
+    for i in range(1, t+1):
+        #A = []
+        all_good = True
+        all_evil = True
+
+        for node in acceptors:
+            m = broadcast(node, mailbox)
+            all_evil &= m == EVIL_MSG
+            all_good &= m == GOOD_MSG
+            #A.append(m)
+
+        for node in proposers:
+            broadcast(node, mailbox)
+
+        update_all(mailbox)
+        mailbox.clear()
+        #assert all_good == all(m == GOOD_MSG for m in A)
+        #assert all_evil == all(m == EVIL_MSG for m in A)
+        if all_good: return True, i
+        if all_evil: return False, i
+    return False, t
